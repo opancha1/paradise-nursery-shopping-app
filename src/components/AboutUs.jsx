7d6792d6bdcc6f70419254338700f1de602f2574
opancha1 <opancha1@charlotte.edu>
2026-09-22 18:21:45 -0400

    Add About Us component

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,18 @@
+export default function AboutUs() {
+  return (
+    <section className="about-page">
+      <div className="about-card">
+        <h1>About Paradise Nursery</h1>
+        <p>
+          Paradise Nursery is an online plant shop dedicated to helping people
+          bring more greenery into their homes. We offer a selection of indoor,
+          succulent, and flowering plants for plant lovers of all experience levels.
+        </p>
+        <p>
+          Our goal is to make choosing and caring for houseplants simple,
+          convenient, and enjoyable.
+        </p>
+      </div>
+    </section>
+  )
+}
